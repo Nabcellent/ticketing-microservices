17aa08becf64f12781d5c4b3a8d5c5185445b7d6
Nabcellent <nabcellent.dev@gmail.com>
2021-12-21 02:31:19 +0300

    Initial commit

---
+++ auth/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import {json} from 'body-parser'
+
+const app = express()
+app.use(json())
+
+app.get('/api/users/current-user', (req, res) => {
+    res.send('Hi There!')
+})
+
+app.listen(3000, () => console.log(`Auth: listening on port 3000!`))
