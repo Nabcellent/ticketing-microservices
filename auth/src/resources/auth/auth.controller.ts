3eee4a943fa6aacbb7010bf9fda9723d1ef56ab6
Nabcellent <nabcellent.dev@gmail.com>
2021-12-24 13:25:00 +0300

    App: Implement user authorization (JWT)
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- auth/src/resources/auth/auth.controller.ts
+++ auth/src/resources/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import {validationResult} from "express-validator";
 import {RequestValidationError} from "../../utils/exceptions/request-validation.error";
 import {User} from "../../models/user";
 import {BadRequestError} from "../../utils/exceptions/bad-request.error";
+import jwt from 'jsonwebtoken'
 
 export async function signUp(req: Request, res: Response) {
     const errors = validationResult(req);
@@ -13,10 +14,19 @@ export async function signUp(req: Request, res: Response) {
 
     const existingUser = await User.findOne({email})
 
-    if(existingUser) throw new BadRequestError('Email already in use.')
+    if (existingUser) throw new BadRequestError('Email already in use.')
 
-    const user  = User.build({email, password})
+    const user = User.build({email, password})
     await user.save()
+
+    //  Generate JWT
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, process.env.JWT_KEY!)
+
+    //  Store JWT on session
+    req.session = {jwt: userJwt}
 
     res.status(201).send(user)
 }
