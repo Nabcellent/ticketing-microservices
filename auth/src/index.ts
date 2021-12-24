e15b133f8b6f2a093caaa4cd993e081545c31331
Nabcellent <nabcellent.dev@gmail.com>
2021-12-24 12:34:53 +0300

    App: Implement user authorization (JWT)
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,10 +4,18 @@ import mongoose from 'mongoose'
 import {authRouter} from "./routes/auth";
 import errorHandler from "./middlewares/error.middleware";
 import {NotFoundError} from "./utils/exceptions/not-found.error";
+import cookieSession from "cookie-session";
 
 const app: Application = express()
+
+app.set('trust proxy', true)
+
 app.use(json())
 app.use(urlencoded({extended: false}))
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}))
 
 app.use('/api', authRouter)
 
