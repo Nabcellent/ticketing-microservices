38fd6028f750a5e5b0839a0da8eab0480487a19f
Nabcellent <nabcellent.dev@gmail.com>
2021-12-31 12:39:37 +0300

    App: Testing isolated microservices
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- auth/src/routes/__test__/auth.test.ts
+++ auth/src/routes/__test__/auth.test.ts
@@ -10,3 +10,52 @@ it('returns a 201 on successful sign up', async () => {
         })
         .expect(201)
 })
+
+it('should return a 400 with an invalid email', function () {
+    return request(app)
+        .post('/api/users/sign-up')
+        .send({
+            email: 'test@testcom',
+            password: 'password'
+        })
+        .expect(400)
+});
+
+it('should return a 400 with an invalid password', function () {
+    return request(app)
+        .post('/api/users/sign-up')
+        .send({})
+        .expect(400)
+});
+
+it('should return a 400 with missing email & or password', async () => {
+    await request(app)
+        .post('/api/users/sign-up')
+        .send({
+            email: 'test@testcom',
+        })
+        .expect(400)
+    await request(app)
+        .post('/api/users/sign-up')
+        .send({
+            password: 'pas'
+        })
+        .expect(400)
+});
+
+it('Disallows duplicate emails', async () => {
+    await request(app)
+        .post('/api/users/sign-up')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201)
+    await request(app)
+        .post('/api/users/sign-up')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(400)
+})
