eda74d4a647bc07d05d8f8931dda5809bf1b090e
Nabcellent <nabcellent.dev@gmail.com>
2022-01-01 14:28:30 +0300

    App: Testing isolated microservices
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

---
+++ auth/src/routes/__test__/sign-out.test.ts
@@ -0,0 +1,17 @@
+import request from "supertest";
+import {app} from "../../app";
+
+it('Clears the cookie after signing out.', async () => {
+    await request(app)
+        .post('/api/users/sign-up')
+        .send({
+            email:'test@test.com',
+            password:'password'
+        }).expect(201)
+
+    const response = await request(app)
+        .post('/api/users/sign-out')
+        .send({}).expect(200)
+
+    expect(response.get('Set-Cookie')[0]).toBeDefined()
+})
