f7f51f9937637f7ebe3339ccc1259b67dab89d6c
Nabcellent <nabcellent.dev@gmail.com>
2022-01-01 19:47:59 +0300

    App: Integrating a server-side rendered react app
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

---
+++ client/pages/auth/sign-in.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useRequest } from "../../hooks/use-request";
+import Router  from "next/router";
+
+export default () => {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const { sendRequest, errors } = useRequest({
+		url: '/api/users/sign-in',
+		method: 'post',
+		body: { email, password },
+		onSuccess: () => Router.push('/')
+	})
+
+	const onSubmit = async event => {
+		event.preventDefault()
+
+		await sendRequest()
+	}
+
+	return (
+		<form action="#" onSubmit={ onSubmit }>
+			<h1>Sign In</h1>
+
+			{ errors }
+
+			<div className="mb-3">
+				<input type="email" className={ 'form-control' } value={ email } autoFocus
+					   onChange={ e => setEmail(e.target.value) } placeholder={ 'Email address' } required/>
+			</div>
+			<div className="mb-3">
+				<input type="password" className={ 'form-control' } value={ password }
+					   onChange={ e => setPassword(e.target.value) } placeholder={ 'password' }
+					   required/>
+			</div>
+			<div className={ 'text-end' }>
+				<button className={ 'btn btn-primary' }>Sign In</button>
+			</div>
+		</form>
+	)
+}
