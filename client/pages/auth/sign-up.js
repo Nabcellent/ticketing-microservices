b312172bcd470557d5dfe331f5b6069755b3dcd6
Nabcellent <nabcellent.dev@gmail.com>
2022-01-01 17:37:32 +0300

    App: Integrating a server-side rendered react app
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- client/pages/auth/sign-up.js
+++ client/pages/auth/sign-up.js
@@ -1,18 +1,38 @@
 import { useState } from "react";
+import axios from "axios";
 
 export default () => {
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
+	const [errors, setErrors] = useState([])
 
-	const onSubmit = event => {
+	const onSubmit = async event => {
 		event.preventDefault()
 
-		console.log(email, password)
+		try {
+			const response = await axios.post('/api/users/sign-up', { email, password })
+
+			console.log(response.data)
+		} catch(err) {
+			setErrors(err.response.data.errors)
+		}
 	}
 
 	return (
-		<form action="#" onSubmit={onSubmit}>
+		<form action="#" onSubmit={ onSubmit }>
 			<h1>Sign Up</h1>
+
+			{
+				errors.length > 0 &&
+				<div className="alert alert-danger alert-dismissible fade show" role="alert">
+					<strong>Oops!</strong>
+					<ul className={ 'my-0' }>
+						{ errors.map(err => <li key={ err.message }>{ err.message }</li>) }
+					</ul>
+					<button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"/>
+				</div>
+			}
+
 			<div className="mb-3">
 				<input type="email" className={ 'form-control' } value={ email } autoFocus
 					   onChange={ e => setEmail(e.target.value) } placeholder={ 'Email address' } required/>
