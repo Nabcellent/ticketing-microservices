0eedb1cdac550ced8c745ad0124414c489a7483f
Nabcellent <nabcellent.dev@gmail.com>
2022-01-01 21:00:40 +0300

    App: Integrating a server-side rendered react app
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import { buildClient } from "../api/build-client";
+import { Component } from "react";
 
-export default ({Component, pageProps}) => {
-	return <Component {...pageProps}/>
+const AppComponent = ({Component, pageProps, currentUser}) => {
+	return (
+		<div>
+			<h1>Header</h1>
+			<Component {...pageProps}/>
+		</div>
+	)
 }
+
+AppComponent.getInitialProps = async({ Component, ctx }) => {
+	const client = buildClient(ctx)
+	const { data } = await client.get('/api/users/current-user')
+
+	let pageProps = {};
+	if(Component.getInitialProps) {
+		pageProps = await Component.getInitialProps(ctx)
+	}
+
+	return data
+}
+
+export default AppComponent
