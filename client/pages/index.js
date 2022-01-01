45df886a6e15ed8697279da2496fd60252d130bb
Nabcellent <nabcellent.dev@gmail.com>
2022-01-01 19:30:48 +0300

    App: Integrating a server-side rendered react app
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,8 +6,16 @@ const Home = ({ currentUser }) => {
 	return <h1>Landing page</h1>
 }
 
-Home.getInitialProps = async() => {
-	const response = await axios.get('/api/users/current-user')
+Home.getInitialProps = async({ req }) => {
+	let response;
+	if(typeof window === 'undefined') {
+		// Request being made from server url should be as follows [http://SERVICENAME.NAMESPACE.svc.cluster.local]
+		response = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/current-user', {
+			headers: req.headers
+		})
+	} else {
+		response = await axios.get('/api/users/current-user')
+	}
 
 	return response.data
 }
