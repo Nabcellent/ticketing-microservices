a9fcb107bfdbd3116390f7dc5a1097331ce8a1b1
Nabcellent <nabcellent.dev@gmail.com>
2022-01-02 16:04:55 +0300

    App: CRUD Server setup
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

---
+++ tickets/routes/__test__/new.test.ts
@@ -0,0 +1,19 @@
+it('should have a route handler listening to /api/tickets for post requests', async function () {
+    
+});
+
+it('should only be accessed if a user is signed in', async function () {
+
+});
+
+it('should return an error if an invalid title is provided', async function () {
+    
+});
+
+it('should return an error if an invalid price is provided', async function () {
+    
+});
+
+it('should create a ticket with valid inputs', async function () {
+    
+}); 
