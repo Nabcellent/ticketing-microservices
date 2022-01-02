13f953754b555d90e730daf7f165515efd40b9fc
Nabcellent <nabcellent.dev@gmail.com>
2022-01-02 20:07:02 +0300

    Update Common lib

--- common/src/middlewares/error.middleware.ts
+++ common/src/middlewares/error.middleware.ts
@@ -8,5 +8,7 @@ export const errorHandler = (error: Error, req: Request, res: Response, next: Ne
         return res.status(error.statusCode).send({errors: error.serializeErrors()})
     }
 
+    console.error(error)
+
     res.status(400).send({errors: [{message}]})
 }
