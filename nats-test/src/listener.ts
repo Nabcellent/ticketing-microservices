9f42f6070a67fb8df5935834d2be0157da2ebad7
Nabcellent <nabcellent.dev@gmail.com>
2022-01-05 18:50:14 +0300

    App: NATS Streaming server - an event bus implementation
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,9 +1,9 @@
-import nats from "node-nats-streaming";
+import nats, {Message} from "node-nats-streaming";
 
 console.clear()
 
 const stan = nats.connect('ticketing', '123', {
-    url:'http://localhost:4222'
+    url: 'http://localhost:4222'
 })
 
 stan.on('connect', () => {
@@ -11,7 +11,9 @@ stan.on('connect', () => {
 
     const subscription = stan.subscribe('ticket:created')
 
-    subscription.on('message', msg => {
-        console.log('Message received.')
+    subscription.on('message', (msg: Message) => {
+        const data = msg.getData()
+
+        if (typeof data === 'string') console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
     })
 })
