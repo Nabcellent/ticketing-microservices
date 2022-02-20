2a7f0e563dffcf2c5789c11c023ab92c6391e21b
nabcellent <nabcellent.dev@gmail.com>
2022-02-20 19:59:15 +0300

    Cross-Service Data Replication In Action
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- orders/src/routes/orders.ts
+++ orders/src/routes/orders.ts
@@ -2,6 +2,7 @@ import {Router} from "express";
 import {requireAuth, ValidateRequest} from "@nabz.tickets/common";
 import {body} from "express-validator";
 import {OrderController} from '../controllers/order.controller';
+import mongoose from 'mongoose';
 
 const router = Router();
 
@@ -9,8 +10,8 @@ router.get('/orders', OrderController.index);
 router.get('/orders/:id', OrderController.show);
 
 router.post('/orders', requireAuth, [
-    body('title').not().isEmpty().withMessage('Title is required.'),
-    body('price').isFloat({gt: 0}).withMessage('Price is required & must be greater than 0.')
+    body('ticket_id').not().isEmpty()
+        .custom((input: string) => mongoose.Types.ObjectId.isValid(input)).withMessage('Ticket Id is required.'),
 ], ValidateRequest, OrderController.store);
 
 router.delete('/orders/:id', requireAuth, OrderController.delete);
