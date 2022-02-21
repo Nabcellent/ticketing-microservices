9706cca3cb1f4569e66b7d440e6174b8e2a8fc8c
nabcellent <nabcellent.dev@gmail.com>
2022-02-21 13:52:27 +0300

    Listening for Events and Handling Concurrency Issues
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- orders/src/events/listeners/ticket-created.listener.ts
+++ orders/src/events/listeners/ticket-created.listener.ts
@@ -1,12 +1,16 @@
 import {Listener, Subject, TicketCreatedEvent} from '@nabz.tickets/common';
 import {Message} from 'node-nats-streaming';
 import {QueueGroupName} from '../queue-group-name';
+import {Ticket} from '../../models/ticket';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
     subject: Subject.TicketCreated = Subject.TicketCreated;
-    queueGroupName = QueueGroupName.OrderService
+    queueGroupName = QueueGroupName.OrderService;
 
-    onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+    async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+        const {id, title, price} = data;
+        await Ticket.create({_id: id, title, price});
 
+        msg.ack();
     }
 }
