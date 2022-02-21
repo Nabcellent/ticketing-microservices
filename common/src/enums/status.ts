e3212c89560874933b495349f928baab296a4c62
nabcellent <nabcellent.dev@gmail.com>
2022-02-21 09:53:59 +0300

    Update Common lib

--- common/src/enums/status.ts
+++ common/src/enums/status.ts
@@ -1,13 +1,13 @@
 export enum Status {
     //  The order has been created and the ticket being ordered has not been reserved.
-    CREATED,
+    ORDER_CREATED = 'CREATED',
 
     //  The ticket being ordered has already been reserved or user has cancelled the order or order expires before payment.
-    CANCELLED,
+    ORDER_CANCELLED = 'CANCELLED',
 
     //  The ticket has successfully been reserved
-    AWAITING_PAYMENT,
+    ORDER_AWAITING_PAYMENT = 'AWAITING_PAYMENT',
 
     //  The ticket has been reserved & the user has made the payment.
-    COMPLETE,
+    ORDER_COMPLETE = 'COMPLETE',
 }
