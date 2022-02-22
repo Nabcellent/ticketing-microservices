ecbfb96f6663630ed9929059c31d6606cbdbea24
nabcellent <nabcellent.dev@gmail.com>
2022-02-22 07:46:38 +0300

    Listening for Events and Handling Concurrency Issues
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

---
+++ orders/src/events/listeners/__test__/ticket-updated.listener.test.ts
@@ -0,0 +1,49 @@
+import {TicketUpdatedListener} from '../ticket-updated.listener';
+import {natsWrapper} from '../../../nats-wrapper';
+import {TicketUpdatedEvent} from '@nabz.tickets/common';
+import mongoose from 'mongoose';
+import {Message} from 'node-nats-streaming';
+import {Ticket} from '../../../models/ticket';
+
+const setup = async () => {
+    //  Create instance of listener
+    const listener = new TicketUpdatedListener(natsWrapper.client);
+
+    //  Create and save a ticket
+    const ticket = await Ticket.create({
+        id: new mongoose.Types.ObjectId().toHexString(),
+        title: 'Concert',
+        price: 20
+    });
+
+    //  Create fake data event
+    const data: TicketUpdatedEvent['data'] = {
+        id: ticket.id,
+        title: 'New Concert',
+        price: 70,
+        user_id: new mongoose.Types.ObjectId().toHexString(),
+        version: ticket.version + 1
+    };
+
+    //  Create fake message object
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn()
+    };
+
+    return {listener, ticket, data, msg};
+};
+
+it('should find, update and save a ticket.', async function () {
+    const {listener, ticket, data, msg} = await setup();
+});
+
+it('should ack() the message.', async function () {
+    const {listener, data, msg} = await setup();
+
+    //  Call the on message func with the data obj + message obj
+    await listener.onMessage(data, msg);
+
+    //  Write assertions to make sure ack() is called
+    expect(msg.ack).toHaveBeenCalled();
+}); 
