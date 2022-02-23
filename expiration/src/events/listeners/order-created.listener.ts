01842ccc8027af45c6a2dfddece778d143e29f1d
nabcellent <nabcellent.dev@gmail.com>
2022-02-23 07:00:30 +0300

    Worker services
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- expiration/src/events/listeners/order-created.listener.ts
+++ expiration/src/events/listeners/order-created.listener.ts
@@ -6,24 +6,6 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     queueGroupName = QueueGroupName.ExpirationService;
 
     async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-        //  Find the ticket being reserved.
-        const ticket = await Ticket.findById(data.ticket.id);
-
-        //  Throw error if ticket doesn't exist
-        if (!ticket) throw new Error('Ticket not found!');
-
-        //  Mark ticket as reserved by setting the order_id prop and save
-        ticket.set({order_id: data.id});
-        await ticket.save();
-
-        await new TicketUpdatedPublisher(this.client).publish({
-            id: ticket.id,
-            user_id: ticket.user_id,
-            order_id: ticket.order_id,
-            title: ticket.title,
-            price: ticket.price,
-            version: ticket.version
-        });
 
         //  ack() the message
         msg.ack();
