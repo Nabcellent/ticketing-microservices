d5bc864fecf68043b93c1309a2c2a821e1a2d466
nabcellent <nabcellent.dev@gmail.com>
2022-02-23 08:35:04 +0300

    Worker services
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

---
+++ orders/src/events/listeners/expiration-complete.listener.ts
@@ -0,0 +1,28 @@
+import {ExpirationCompleteEvent, Listener, QueueGroupName, Status, Subject} from '@nabz.tickets/common';
+import {Message} from 'node-nats-streaming';
+import {Order} from '../../models/order';
+import {OrderCancelledPublisher} from '../publishers/order-cancelled.publisher';
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+    subject: Subject.ExpirationComplete = Subject.ExpirationComplete;
+    queueGroupName = QueueGroupName.ExpirationService;
+
+    async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+        const order = await Order.findById(data.order_id).populate('ticket');
+
+        if (!order) throw new Error('Order not found!');
+
+        order.set({status: Status.ORDER_CANCELLED});
+        await order.save();
+
+        await new OrderCancelledPublisher(this.client).publish({
+            id: order.id,
+            version: order.version,
+            ticket: {
+                id: order.ticket.id
+            }
+        });
+
+        msg.ack()
+    }
+}
