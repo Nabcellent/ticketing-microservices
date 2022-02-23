bb88d4d7bbf9e9d2f98b9531610072a3f7d3ed52
nabcellent <nabcellent.dev@gmail.com>
2022-02-23 17:04:24 +0300

    Handling Payments
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- payments/src/controllers/payment.controller.ts
+++ payments/src/controllers/payment.controller.ts
@@ -1,7 +1,17 @@
 import {Request, Response} from "express";
+import {Order} from '../models/order';
+import {BadRequestError, NotAuthorizedError, NotFoundError, Status} from '@nabz.tickets/common';
 
 export const PaymentController = {
     store: async (req: Request, res: Response) => {
+        const {token, order_id} = req.body;
+
+        const order = await Order.findById(order_id);
+
+        if (!order) throw new NotFoundError();
+        if (order.user_id !== req.currentUser!.id) throw new NotAuthorizedError();
+        if (order.status === Status.ORDER_CANCELLED) throw new BadRequestError('Cannot pay for a cancelled order!');
+
         res.status(201).send({success: true});
     },
 };
