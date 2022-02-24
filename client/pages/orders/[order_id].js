0e30e8dd33d610851d15709a9a937e23fff5d8da
nabcellent <nabcellent.dev@gmail.com>
2022-02-24 10:23:35 +0300

    Back to the client
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- client/pages/orders/[order_id].js
+++ client/pages/orders/[order_id].js
@@ -1,9 +1,35 @@
-const OrderShow = () => {
+import {useEffect, useState} from 'react';
+
+const OrderShow = ({order}) => {
+    const [timeLeft, setTimeLeft] = useState(0);
+
+    useEffect(() => {
+        const findTimeLeft = () => {
+            const msLeft = new Date(order.expires_at) - new Date();
+
+            setTimeLeft(Math.round(msLeft / 1000));
+        };
+
+        findTimeLeft();
+        const timerId = setInterval(findTimeLeft, 1000);
+
+        return () => clearInterval(timerId);
+    }, [order]);
+
     return (
         <div>
-            Order Show
+            {
+                timeLeft < 0 ? <div>Order expired!</div> : <div>You have <strong>{timeLeft}s</strong> to pay.</div>
+            }
         </div>
-    )
-}
+    );
+};
 
-export default OrderShow
+OrderShow.getInitialProps = async (context, client) => {
+    const {order_id} = context.query;
+    const {data} = await client.get(`/api/orders/${order_id}`);
+
+    return {order: data};
+};
+
+export default OrderShow;
