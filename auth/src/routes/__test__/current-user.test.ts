b164cd1ccca7abfb1a87f60ab97c407c4d57ad4c
nabcellent <nabcellent.dev@gmail.com>
2022-02-25 06:55:00 +0300

    Deployment: CI-CD
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -8,7 +8,7 @@ it('should respond with details about current user',async function () {
     const response = await request(App)
         .get('/api/users/current-user')
         .set('Cookie', cookie)
-        .send().expect(200)
+        .send().expect(400)
 
     expect(response.body.currentUser.email).toEqual('test@test.com')
 });
